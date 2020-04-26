7061d954e849f3ba1feac33425682c4bdfca8110
steveyalcin <steven.yalcin@gmail.com>
2020-04-26 09:40:57 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Spacex from './components/Spacex';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    const result = await fetch('https://api.spacexdata.com/v3/launches/latest');
+
+    result.json().then(result => setData(result));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {Object.keys(data)
+            .slice(0, 1)
+            .map(ss => (
+              <div key={ss}>
+                <div className="container">
+                  <img
+                    src={data.links.mission_patch}
+                    className="App-logo"
+                    alt="logo"
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
       </header>
+      <div>
+        <Spacex />
+      </div>
     </div>
   );
 }
---
+++ src/components/Spacex.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Spacex() {
+  return <div></div>;
+}
