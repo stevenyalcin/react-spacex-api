9db0ae9f707dc22f96edd86c5ea34ba87cdaf9c4
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-10-13 15:19:07 -0400

    front end redesigned and implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Spacex from './components/Spacex';
-import AuthenticationAPI from '../src/components/Auth';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AuthenticationAPI from "./Auth";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   async function fetchData() {
     const result = await fetch(AuthenticationAPI.API_URL());
-
-    result.json().then(result => setData(result));
+    result.json().then((result) => {
+      if (result) {
+        setData(result);
+        setLoading(false);
+      }
+    });
   }
 
   useEffect(() => {
@@ -19,113 +23,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          {Object.keys(data)
-            .slice(0, 1)
-            .map(ss => (
-              <div key={ss}>
-                <div className="container">
-                  <img
-                    src={data.links.mission_patch}
-                    className="App-logo"
-                    alt="logo"
-                  />
-                </div>
-              </div>
-            ))}
+        <div className="container">
+          {loading ? (
+            <div className="loader"></div>
+          ) : (
+            <div className="flex-container">
+              {data &&
+                data.slice(0, 12).map((ss, index) => (
+                  <div key={index}>
+                    <div>
+                      <img
+                        src={ss.links.mission_patch}
+                        className="App-logo"
+                        alt="logo"
+                      />
+                    </div>
+                    <button className="button button1">Learn more</button>
+                  </div>
+                ))}
+            </div>
+          )}
         </div>
       </header>
-      <div className="App-rockets"></div>
-
-      <div className="App-mission">
-        {Object.keys(data)
-          .slice(0, 1)
-          .map(ss => (
-            <div key={ss}>
-              <div className="container">
-                <div className="mission-details">
-                  {data.details}
-                  <div>
-                    <a href={data.links.presskit}>
-                      Please click for Mission overview
-                    </a>
-                  </div>
-                </div>
-                <Spacex
-                  flight_number={data.flight_number}
-                  mission_name={data.mission_name}
-                  launch_date_utc={data.launch_date_utc}
-                  rocket_name={data.rocket.rocket_name}
-                  core_serial={data.rocket.first_stage.cores.map(
-                    items => items.core_serial
-                  )}
-                  flight={data.rocket.first_stage.cores.map(
-                    items => items.flight
-                  )}
-                  block={data.rocket.first_stage.cores.map(
-                    items => items.block
-                  )}
-                  gridfins={data.rocket.first_stage.cores.map(
-                    items => items.gridfins
-                  )}
-                  reused={data.rocket.first_stage.cores.map(
-                    items => items.reused
-                  )}
-                  land_success={data.rocket.first_stage.cores.map(
-                    items => items.land_success
-                  )}
-                  landing_intent={data.rocket.first_stage.cores.map(
-                    items => items.landing_intent
-                  )}
-                  site_name={data.launch_site.site_name}
-                />
-              </div>
-            </div>
-          ))}
-      </div>
-      <div className="falcon-images">
-        {Object.keys(data)
-          .slice(0, 1)
-          .map(ss => (
-            <div key={ss}>
-              <div className="mission-images">
-                {data.links.flickr_images.slice(1, 2).map(items => {
-                  return (
-                    <img
-                      src={items}
-                      key={items}
-                      alt=""
-                      height="33%"
-                      width="33%"
-                    />
-                  );
-                })}
-                {data.links.flickr_images.slice(3, 4).map(items => {
-                  return (
-                    <img
-                      src={items}
-                      key={items}
-                      alt=""
-                      height="33%"
-                      width="33%"
-                    />
-                  );
-                })}
-                {data.links.flickr_images.slice(4, 5).map(items => {
-                  return (
-                    <img
-                      src={items}
-                      key={items}
-                      alt=""
-                      height="33%"
-                      width="33%"
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          ))}
-      </div>
     </div>
   );
 }
--- src/Auth.js
+++ src/Auth.js
@@ -1,7 +1,7 @@
 const spacexDataService = {
   API_URL() {
-    return 'https://api.spacexdata.com/v3/launches/latest';
-  }
+    return "https://api.spacexdata.com/v3/launches/";
+  },
 };
 
 export { spacexDataService as default };
